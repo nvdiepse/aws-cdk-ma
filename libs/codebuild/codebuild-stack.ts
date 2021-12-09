09e4cf1955550cda2d22ac25dbd713c8f892fac7
nvdiepse <nvdiepse@gmail.com>
2021-12-09 16:21:11 +0700

    update

--- libs/codebuild/codebuild-stack.ts
+++ libs/codebuild/codebuild-stack.ts
@@ -2,6 +2,7 @@ import { Vpc } from '@aws-cdk/aws-ec2';
 import * as cdk from '@aws-cdk/core';
 import { LinuxBuildImage, Project, BuildSpec } from '@aws-cdk/aws-codebuild';
 import { getServiceName } from '../../helper/common';
+import { Role, ServicePrincipal } from '@aws-cdk/aws-iam';
 
 export interface CodeBuildStackProps extends cdk.StackProps {
   readonly vpc: Vpc;
@@ -15,6 +16,14 @@ export class CodeBuildStack extends cdk.Stack {
   }
 
   public initCodebuild() {
+    // codeBuildIamPrincipal is shared across stacks.
+    const codeBuildIamPrincipal = 'site-publisher';
+
+    // Create a role for our Codebuild so it can be used by other stacks.branchOrRef: '*', // * Covers all branches, tags, commit IDs, etc...
+    const sitePublisherCodeBuild = new Role(this, 'Role', {
+      assumedBy: new ServicePrincipal('codebuild.amazonaws.com'),
+      roleName: codeBuildIamPrincipal,
+    });
     new Project(this, 'codebuild', {
       buildSpec: BuildSpec.fromObject({
         version: '0.2',
@@ -30,15 +39,12 @@ export class CodeBuildStack extends cdk.Stack {
               'node --version',
               'git --version',
               'git clone https://github.com/nvdiepse/aws-cdk-ma.git',
-              "cd aws-cdk-ma && npm install",
-              'cdk --version',
-              "cdk ls",
-              "cdk deploy WebStack --require-approval never"
+              'cd aws-cdk-ma && npm install',
+              'cdk deploy WebStack --require-approval never',
 
               // "export VERSION=$(date +\\%Y\\%m\\%d\\%H\\%M\\%S)",
               // "git clone https://github.com/nvdiepse/aws-cdk-ma.git",
               // "cd aws-cdk-ma && npm install",
-              
             ],
           },
         },
@@ -47,6 +53,7 @@ export class CodeBuildStack extends cdk.Stack {
         buildImage: LinuxBuildImage.STANDARD_5_0,
       },
       projectName: `${getServiceName('codebuild')}`,
+      role: sitePublisherCodeBuild,
     });
   }
 }
