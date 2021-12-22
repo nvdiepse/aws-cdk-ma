66e6fe398b6118a8a1ac01ddecd0e2702278749f
nvdiepse <nvdiepse@gmail.com>
2021-12-23 01:49:06 +0700

    update push ecr

--- push_image_to_ecr.sh
+++ push_image_to_ecr.sh
@@ -2,7 +2,6 @@ source $(dirname $0)/variable.config
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ECR
 
-docker pull node:14.1.0-alpine
-docker tag node:14.1.0-alpine $AWS_ECR/$SERVICE_NAME
-docker push $AWS_ECR/$SERVICE_NAME
+docker tag $SERVICE_NAME:latest $AWS_ECR/$SERVICE_NAME:latest
+docker push $AWS_ECR/$SERVICE_NAME:latest
 
