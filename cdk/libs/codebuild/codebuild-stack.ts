fb12e5451acb8df3e91cf28f3b3e9e07c3fc987f
nvdiepse <nvdiepse@gmail.com>
2022-01-10 21:28:35 +0700

    update

--- cdk/libs/codebuild/codebuild-stack.ts
+++ cdk/libs/codebuild/codebuild-stack.ts
@@ -43,6 +43,11 @@ export class CodeBuildStack extends cdk.Stack {
         phases: {
           build: {
             commands: [
+              'yum update',
+              'yum install -y docker',
+              'curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose',
+              'chmod +x /usr/local/bin/docker-compose',
+              'docker-compose version',
               'apt update -y',
               'apt install nodejs -y',
               'npm i -g aws-cdk',
